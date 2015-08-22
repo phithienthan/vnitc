4448e82e5448feb8563c6e85a0961e6e1b0db54a
Nguyen Dang Quyet <ndquyet@yahoo.com>
2015-08-22 10:23:14 +0700

    Create lastest product

--- app/modules/default/controllers/indexController.php
+++ app/modules/default/controllers/indexController.php
@@ -12,7 +12,12 @@ Class indexController Extends defaultController
         $articleModel = $this->model->get('article');
         $articles = $articleModel->getByChannel(61, 1);
         $this->view->data['articles'] = $articles;
-
+        
+        // Get lastest product
+        $productModel = $this->model->get('product');
+        $products = $productModel->getHotProducts(24);
+        $this->view->data['products'] = $products;
+        
         $this->view->show('index');
         /* load layout template */
     }
